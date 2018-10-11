7ff5401faf0355f99509168e820202c0a0d8ee21
Adam Barton <adam.barton333@gmail.com>
2018-10-11 21:53:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
         elsif operation = "multiply"
         content = num1 * num2
         else
-        content = num1 / num2
+        content = num2 / num1
         end
         content.to_s
     end

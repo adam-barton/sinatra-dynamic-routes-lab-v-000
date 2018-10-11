2881e8bbcf1d6c49ce0ce14a015ed26b3ce189cf
Adam Barton <adam.barton333@gmail.com>
2018-10-11 21:03:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,8 @@ class App < Sinatra::Base
       end
 
     get '/say/:word1/:word2/:word3/:word4/:word5' do
-      "#{word1} #{word2} #{word3} #{word4} #{word5}."
+      "Hello"
+      #"#{word1} #{word2} #{word3} #{word4} #{word5}."
     end
 
     get '/:operation/:number1/:number2' do
